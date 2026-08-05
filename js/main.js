67b121dd89cd70e84e86b45d989d12b57a7b36c9
akinyodeolamide <akinyodeolamide27@gmail.com>
2026-08-05 09:37:45 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -167,9 +167,7 @@
     hashPassword(password) {
       let hash = 0;
       for (let i = 0; i < password.length; i++) {
-        const char = password.charCodeAt(i);
-        hash = ((hash << 5) - hash) + char;
-        hash = hash & hash;
+        hash = ((hash * 31) + password.charCodeAt(i)) >>> 0;
       }
       return hash.toString(16);
     },
